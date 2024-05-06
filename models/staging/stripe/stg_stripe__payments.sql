5665491173816748f8e4953f17d7327529205a0f
dmhsmh@cox.net <dmhsmh@cox.net>
2024-05-06 00:52:51 +0000

    dbt sources module

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,6 +2,7 @@ select
     id as payment_id,
     order_id,
     payment_method,
-    amount / 100 as amount
+    amount / 100 as amount,
+    _batched_at
 
-from raw.stripe.payments
+from {{ source('stripe', 'payments') }}
